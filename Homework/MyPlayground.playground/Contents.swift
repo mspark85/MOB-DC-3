559bd39780330f0ea0d0a7ebd4f84d3bd0cedb1d
mspark85 <Kevin@Mins-MacBook-Pro.local>
2015-06-10 18:48:41 -0400

    I created an empty project to make this work

---
+++ Homework/MyPlayground.playground/Contents.swift
@@ -0,0 +1,18 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+let maximumNumberofLoginAttempts = 10
+var currentLoginAttempt = 0
+var x = 0.0, y = 0.0, z=0.0
+var welcomeMessage: String
+welcomeMessage = "Hello"
+var red, green, blue: Double
+var friendWelcome = "Hello"
+friendWelcome = "Bonjour!"
+println(friendWelcome)
+print(friendWelcome)
+println("This is a string")
+println("The current value of friendWelcome is \(friendWelcome)")
+
