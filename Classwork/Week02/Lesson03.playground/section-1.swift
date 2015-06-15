519033ace13f4175fa638bca2bb61d485b2504b7
mspark85 <Kevin@Mins-MacBook-Pro.local>
2015-06-15 18:19:20 -0400

    Moving towards to the new git structure

--- Classwork/Week02/Lesson03.playground/section-1.swift
+++ Classwork/Week02/Lesson03.playground/section-1.swift
@@ -4,8 +4,8 @@ import Foundation
 
 // TODO: Create two variables, name and age. Name is a string, age is an integer.
 
-var name: String = "Kevin"
-var age: Int = 29
+let name: String = "Kevin"
+let age: Int = 29
 
 
 // TODO: Print "Hello {whatever the value of name is}, you are {whatever the value of age is} years old!"
@@ -15,31 +15,30 @@ println ("Hello \(name), you are \(age) years old!")
 
 // TODO: Print “You can drink” below the above text if the user is above 21. If they are above 18, print “you can vote”. If they are above 16, print “You can drive”
 
-if age > 21 {
+if age >= 21 {
     println ("You can drink")
 }
 
-if age > 18 {
+if age >= 18 {
     println ("You can vote")
 }
 
-if age > 16 {
+if age >= 16 {
     println ("You can drive")
 }
 
 // TODO: Print “you can drive” if the user is above 16 but below 18. It should print “You can drive and vote” if the user is above 18 but below 21. If the user is above 21, it should print “you can drive, vote and drink (but not at the same time!”.
 
-if age > 16 && age < 18 {
+if age >= 16 && age < 18 {
     println("you can drive")
-}
-
-if age > 18 && age < 21 {
+} else if age >= 18 && age < 21 {
     println("You can drive and vote")
+} else if age >= 21 {
+    println ("you can drive, vote and drink (but not at the same time)!")
+} else {
+    println ("you are too young to do anything fun")
 }
 
-if age > 21 {
-    println ("you can drive, vote and drink (but not at the same time)!")
-}
 
 // TODO: Print the first fifty multiples of seven minus one (e.g. the first three multiples are 7, 14, 21. The first three multiples minus one are 6, 13, 20)
 
@@ -67,23 +66,20 @@ if number%2 == 0 {
 
 // TODO: Print out "Hello {whatever the value of name is}, your name is {however long the string name is} characters long!. Use countElements()
 
-println ("Hello \(name), your name is \(count(name)) characters long!.")
+println ("Hello \(name), your name is \(count(name)) characters long!")
 
 
 
 // TODO: Print the sum of one hundred random numbers. Use rand() to generate random numbers
 
+var n = 1
+var sum = 0
 
-var num = 1
-var randomN = rand()
-
-
-
-
-
-
-
-
+while n <= 100 {
+    sum = sum + Int(rand())
+    println (sum)
+    ++n
+}
 
 // Bonus TO DO: Write a program that prints the numbers from 1 to 100. But for multiples of three print “Fizz” instead of the number and for the multiples of five print “Buzz”. For numbers which are multiples of both three and five print “FizzBuzz”.
 
---
+++ Homework/Reading Practice/Reading 06152015.playground/Contents.swift
@@ -0,0 +1,79 @@
+let names = ["Anna", "Alex", "Brian", "Jack"]
+for name in names {
+    println ("Hello, \(name)!")
+}
+
+
+
+let breakfastCost = 10
+
+switch breakfastCost {
+case 0:
+    print ("Free?! I'll take it!")
+case 1...4:
+    print ("Sounds good. I will buy it")
+case 5...7:
+        ("Grumble. I guess.")
+case 8...10:
+    print ("I'm going elsewhere")
+default:
+    print ("Highway robbery!")
+}
+
+let likeCats: Bool = true
+likeCats
+let numberofCatsOwned: Int = 25
+let dollarsCatsCostMe: Double = Double (numberofCatsOwned) * 500.0
+
+let balloon = (12.0, "Red")
+let (diameter, color) = balloon
+println ("The ballon diamerter is \(diameter)")
+println ("The ballon color is \(color)")
+
+/*
+var 1bNeighborhoods = ["Downtown", "Bixby Knolls", "North Long Beach". "Wilmore", "California Heights"]
+
+for hood in 1bNeighborhoods {
+    if hood == "North Long Beach" {
+        println ("\(hood) is home!")
+    }
+    else {
+        println(hood)
+    }
+}
+*/
+
+var contacts = ["Jeff" : "201-555-6666", "Joe" : "212-555-5666"]
+
+var obamaphone = contacts["Joe"]
+print(obamaphone)
+
+var outsideVariable = 1
+
+class ExampleClass {
+    
+    var insideClassVariable = 2
+    
+    func exampleFunction(functionVariable:Int) -> (Int) {
+        var insideFunctionVariable = 3 + functionVariable
+        
+        print(insideFunctionVariable)
+        print(functionVariable)
+        print(self.insideClassVariable)
+        print(outsideVariable)
+        
+        return (insideFunctionVariable)
+    }
+}
+
+let example = ExampleClass()
+let responseFromFunction = example.exampleFunction(4)
+
+print(outsideVariable)
+
+
+for index in 1...5 {
+    println ("\(index) tims 5 is \(index*5)")
+}
+
+
