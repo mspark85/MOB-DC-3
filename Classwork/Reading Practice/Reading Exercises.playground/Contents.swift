5814da0db10f8376afd49cfbcaae32b7dd921f34
mspark85 <Kevin@Mins-MacBook-Pro.local>
2015-07-06 18:29:38 -0400

    prelesson 10

---
+++ Classwork/Reading Practice/Reading Exercises.playground/Contents.swift
@@ -0,0 +1,6 @@
+// Arrays: stores values of the same type in an ordered list
+    //Array<SomeType> or [SomeType]
+var favoriteGenres: Set<String> = ["Rock", "Classical", "Hiphop"]
+// Dictionaries 
+var namesOfIntegers = [Int: String]()
+var airports: [String : String] = ["XYZ": "Toronto", "DUB": "Dublin"]
